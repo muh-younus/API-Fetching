ce33186a76e7fbb0ed32ab85c22503537a736591
muh-younus <braq369@gmail.com>
2025-07-26 14:13:20 -0700

    enhace the UI

--- src/Axios-fetching/Axios.jsx
+++ src/Axios-fetching/Axios.jsx
@@ -49,16 +49,18 @@ function Axios() {
   return (
     <>
 
-    <div className="h-screen bg-blue-600">
+    {/* <h1 className="bg-gray-300 p-5 rounded-xl w-[400px] ">This is My Title Page</h1> */}
 
-        <h1 className="text-blue-600 ">Hello</h1>
+    <div className=" text-center grid grid-cols-3 place-items-center h-screen mx-auto bg-gray-500 ">
+
+        
         
 
       {data1.map((item, index) => {
 
         return(
  
-    <div key={index} className="bg-blue-500">
+    <div key={index} className=" w-[300px] text-center bg-gray-300  shadow-xl p-3 rounded-xl text-center m-3  h-[100px]">
       <h3>{item.title || "No title"}</h3>
     </div>
 
